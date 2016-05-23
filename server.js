60a049b02723939c157abd3b8aa1ae7c090de17a
hsine55 <hsine.benmessaoud@gmail.com>
2016-05-23 08:58:48 +0000

    refactoring

--- server.js
+++ server.js
@@ -6,38 +6,33 @@ var app = express();
 
 app.get('/*', function(req, res) {
   var longUrl = req.originalUrl.substring(1) ;
-  console.log(longUrl)
-  var dataObject = {} ;
-  if (! isUrlValid(longUrl) ) {
-    dataObject.error = "The passed Url is not valid !"
-  } else {
-    //dataObject = shortUrl(longUrl);
-    Url.findOne({_id: longUrl}, function (err, doc){
-      if (doc){
-      // URL already shortened => redirect to old URL
-        res.redirect(doc.long_url);
-      } else {
-        //  new URL 
-        var newUrl = Url({
-        long_url: longUrl
-        });
-        
-        // save the new link
-        newUrl.save(function(err) {
-          if (err){
-            console.log(err);
-          }
-        });
-        
-        var shortUrl = config.url + "/" + newUrl._id ;
-        dataObject.longUrl = longUrl ;
-        dataObject.shortUrl = shortUrl ;
-        res.send(dataObject);
+  Url.findOne({_id: longUrl}, function (err, doc){
+    if (doc){
+    // URL already shortened => redirect to old URL
+      res.redirect(doc.long_url);
+    } else {
+      //  new URL
+      var dataObject = {} ; //will contain the result
+      dataObject.longUrl = longUrl ;
+      if (! isUrlValid(longUrl) ) { // if url not valid we send it in the response with an error message
+        dataObject.error = "The passed Url is not valid !"
+        res.send(dataObject) ;
+        return ;
       }
-      
-    })
-    
-  }
+      //url is valid 
+      Url.findOne({long_url: longUrl}, function (err, doc){
+        if (doc) { // we already have an entry for this url so just configure the res and send it
+          dataObject.shortUrl = config.url + "/" + doc._id ;
+        } else { // create new entry
+            dataObject.shortUrl = shortenUrl(longUrl) ;
+        }
+        res.send(dataObject);
+      })
+
+    }
+  })
+  
+   
 })
 
 app.listen(process.env.PORT);
@@ -48,7 +43,25 @@ app.listen(process.env.PORT);
  * returns true if Url is valid, false otherwise.
  **/
 function isUrlValid(longUrl) {
-  return true ;
+   var pattern = new RegExp('^https?://(www)\.[A-Za-z0-9_]+\.com$');
+  return pattern.test(longUrl);
 }
 
-
+/**
+ * this function will create a new entry in db for the long url passed in params
+ * return the short url
+ */
+function shortenUrl(longUrl) {
+  var newUrl = Url({
+              long_url: longUrl
+            });
+  // save the new link in the db
+  newUrl.save(function(err) {
+    if (err){
+      console.log(err);
+    }
+  });
+  // the short url will just be the id of the saved url in the db. Todo : optimize this.  
+  var shortUrl = config.url + "/" + newUrl._id ;
+  return shortUrl ;
+}
